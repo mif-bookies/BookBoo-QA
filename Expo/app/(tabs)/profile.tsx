ae3db1d31a1175fc360c79a47e02ad3970219443
Airidas Žaliauskas <air.airidas1@gmail.com>
2024-05-17 14:51:14 +0300

    update: Profile Page UI enhancements

--- Expo/app/(tabs)/profile.tsx
+++ Expo/app/(tabs)/profile.tsx
@@ -1,20 +1,17 @@
-import { useAuth } from "@clerk/clerk-expo";
+import { useAuth, useUser } from "@clerk/clerk-expo";
 import { router } from "expo-router";
 import React, { useEffect } from "react";
-import { Button, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 
 const Page = () => {
   const { isLoaded, signOut, isSignedIn } = useAuth();
+  const { user } = useUser();
 
   useEffect(() => {
     if (isLoaded && !isSignedIn) {
       router.replace("/login");
     }
-  }, []);
-
-  if (!isLoaded) {
-    return null;
-  }
+  }, [isLoaded, isSignedIn]);
 
   const handleSignOut = async () => {
     try {
@@ -25,15 +22,28 @@ const Page = () => {
     }
   };
 
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <View>
+    <View className="flex-1 justify-center items-center">
       {isSignedIn && (
-        <Button
-          title="Sign Out"
-          onPress={() => {
-            handleSignOut();
-          }}
-        />
+        <>
+          <Image
+            source={{ uri: user?.imageUrl }}
+            className="w-32 h-32 rounded-full mb-4"
+          />
+          <Text className="text-xl font-bold mb-4">{user?.fullName}</Text>
+          <Pressable
+            android_ripple={{ color: "rgba(0,0,0,0.1)" }}
+            className=" flex items-center justify-center h-11 bg-blue-500 rounded-lg px-12 py-2 text-white text-sm font-medium transition-colors"
+            style={{ borderRadius: 8 }}
+            onPress={handleSignOut}
+          >
+            <Text className="text-white text-md font-medium">Sign Out</Text>
+          </Pressable>
+        </>
       )}
     </View>
   );
